b5338f12841fba45628ccab8033c73ec131da3f5
shuemer <sigrid.huemer@iteratec.com>
2023-12-01 13:43:22 +0100

    complete day1

--- 2023/src/Day01.kt
+++ 2023/src/Day01.kt
@@ -1,15 +1,30 @@
 fun main() {
+
+    fun extract2DigitNum(line: String): Int {
+        var result = line.filter { it.isDigit() }
+
+        return when (result.length) {
+            1 -> "$result$result".toInt()
+            else -> "${result.first()}${result.last()}".toInt()
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.fold(0) { acc, line -> acc + extract2DigitNum(line) }
+    }
+
+    fun replaceNumbers(input: String): String {
+        // keeping the first and last letters for cases like: "oneight" or "sevenine"
+        return input.replace("one", "o1e").replace("two", "t2o").replace("three", "t3e").replace("four", "f4r")
+            .replace("five", "f5e").replace("six", "s6x").replace("seven", "s7n").replace("eight", "e8t")
+            .replace("nine", "n9e")
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        var replacedInput = input.map { line -> replaceNumbers(line) }
 
-    // test if implementation meets criteria from the description, like:
-    // val testInput = readInput("Day01_test")
-    // check(part1(testInput) == 1)
+        return replacedInput.fold(0) { acc, line -> acc + extract2DigitNum(line) }
+    }
 
     val input = readInput("Day01")
     part1(input).println()
